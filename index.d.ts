c15f43531d72bfefceaf92cc246df69718b379d1
Christopher Deutsch <cd@cdeutsch.com>
2018-12-20 11:27:45 -0600

    Get typings working.

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,7 @@
-// Type definitions for pretty-bytes 5.1
-// Project: https://github.com/sindresorhus/pretty-bytes
+// Type definitions for pretty-bytes-es5 5.1
+// Project: https://github.com/cdeutsch/pretty-bytes-es5
 // Definitions by: York Yao <https://github.com/plantain-00>
 //                 Daniela Yassuda <https://github.com/danielasy>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare function PrettyBytes(number: number, options?: PrettyBytes.PrettyBytesOptions): string;
 export = PrettyBytes;
